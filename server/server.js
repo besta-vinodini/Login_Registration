8d294acdc358f2999b529e3c97565cca68b84ac0
bestavinodini <bestavinodini@gmail.com>
2025-09-16 12:28:52 +0530

    Deploy ready

--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173", // local Vite
+    "https://login-registration-liart.vercel.app" // deployed frontend
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 // DB Connection
 connectDB();
